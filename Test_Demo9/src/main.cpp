6c725f194e160ccded9d24113ba90a241bc0caec
Culturenotes <505106035@qq.com>
2021-01-07 23:17:50 +0800

    new

---
+++ Test_Demo9/src/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include "../config/config.hpp"
+
+using namespace std;
+
+#ifdef HAVE_POW
+    #include <math.h>
+#else
+    #include "../MyMath/MathFunctions.h"
+#endif
+int main(int argc, char *argv[])
+{
+    if (argc < 3){
+        printf("%s Version %d.%d\n",
+            argv[0],
+            Demo_VERSION_MAJOR,
+            Demo_VERSION_MINOR);
+        printf("Usage: %s base exponent \n", argv[0]);
+        return 1;
+    }
+    double base = atof(argv[1]);
+    int exponent = atoi(argv[2]);
+    
+#ifdef HAVE_POW
+    printf("Now we use the standard library. \n");
+    double result = pow(base, exponent);
+#else
+    printf("Now we use our own Math library. \n");
+    double result = power(base, exponent);
+#endif
+
+    printf("%g ^ %d is %g\n", base, exponent, result);
+    return 0;
+}
